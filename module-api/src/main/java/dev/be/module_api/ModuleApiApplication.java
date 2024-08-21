061d08ed1521a827f6db56a45f62706bfb1e1226
kmindev <kangkm0324@gmail.com>
2024-08-21 16:25:21 +0900

    api 모듈 생성
    settings.gradle은 root 프로젝트의 settings.gradle을 사용하기 때문에 제거(제거 안하면, 의존성 추가할 때 에러 발생)

---
+++ module-api/src/main/java/dev/be/module_api/ModuleApiApplication.java
@@ -0,0 +1,13 @@
+package dev.be.module_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleApiApplication.class, args);
+	}
+
+}
